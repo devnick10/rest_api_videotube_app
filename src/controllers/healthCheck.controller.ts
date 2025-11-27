6e6d85a21044db22e87284c4043303ccebe24ec7
devnick10 <nickbhoyar683@gmail.com>
2025-11-28 00:55:03 +0530

    chore:add request type and fix early return

--- src/controllers/healthCheck.controller.ts
+++ src/controllers/healthCheck.controller.ts
@@ -1,10 +1,10 @@
+import { Request } from "express";
 import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 
-const healthcheck = asyncHandler(async (req, res) => {
-  return res
-    .status(200)
-    .json(new ApiResponse(200, "OK", "Health checked passed"));
+const healthcheck = asyncHandler<Request>(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, "OK", "Health checked passed"));
+  return;
 });
 
 export default healthcheck;
