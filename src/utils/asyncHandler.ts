d8726562ee6408ae3e5ae7b7c0fb81c22b6ddd78
devnick10 <nickbhoyar683@gmail.com>
2025-11-30 13:08:58 +0530

    chore: add correct types

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -4,9 +4,9 @@ export function asyncHandler<
   Req extends Request = Request,
   Res extends Response = Response,
 >(
-  handler: (req: Req, res: Res, next: NextFunction) => Promise<any>
+  handler: (req: Req, res: Res, next: NextFunction) => Promise<void>
 ): RequestHandler {
-  return (req: any, res: any, next: NextFunction) => {
-    Promise.resolve(handler(req, res, next)).catch(next);
+  return (req, res, next: NextFunction) => {
+    Promise.resolve(handler(req as Req, res as Res, next)).catch(next);
   };
 }
